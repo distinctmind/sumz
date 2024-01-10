8927d8e3a7694e0ca7dbf0f07c45aba66c58a465
Jeanlouis <jeanlouisrebello@gmail.com>
2024-01-10 14:19:08 -0500

    Final Demo.

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,5 +1,6 @@
 import {useState, useEffect} from 'react';
 import { copy, linkIcon, loader, tick } from '../assets';
+import { useLazyGetSummaryQuery } from '../services/article';
 
 const Demo = () => {
 
@@ -8,8 +9,37 @@ const Demo = () => {
     summary: '',
   });
 
+  const [allArticles, setAllArticles] = useState([]);
+  const [copied, setCopied] = useState('');
+
+  const [getSummary, {error, isFetching}] = useLazyGetSummaryQuery();
+
+  useEffect(()=>{
+    const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'));
+    if (articlesFromLocalStorage) setAllArticles(articlesFromLocalStorage);
+  },[])
+
   const handleSubmit = async (e) => {
-    alert('Submitted');
+    e.preventDefault();
+    const { data } = await getSummary({articleUrl: article.url});
+    if (data?.summary) {
+      
+      const newArticle = {...article, summary: data.summary};
+      setArticle(newArticle);
+      
+      const updatedAllArticles = [newArticle, ...allArticles];
+      setAllArticles(updatedAllArticles);
+
+      localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
+      console.log(newArticle);
+
+    }
+  }
+
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(()=> setCopied(false), 3000)
   }
 
   return (
@@ -42,8 +72,52 @@ const Demo = () => {
           </button>
         </form>
         {/* Browser URL History */}
+        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+          {allArticles.map((article, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={()=> setArticle(article)}
+              className='link_card'
+            >
+              <div className='copy_btn' onClick={() => handleCopy(article.url)}>
+                <img 
+                  src={copied === article.url ? tick : copy} 
+                  alt='copy_icon'
+                  className='w-[40%] h-[40%] object-contain'
+                />
+              </div>
+              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+                {article.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
       {/* Display Results */}
+      <div className='my-10 max-w-full flex justify-center items-center'>
+        {isFetching ? (
+          <img src={loader} alt='loader' className='w-20 h-20 object-contain'/>
+        ) : error ? (
+          <p className='font-inter font-bold text-black text-center'>
+            Well, that wasn't supposed to happen...
+            <br/>
+            <span className='font-satoshi font-normal text-gray-700'>
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className='flex flex-col gap-3'>
+              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                Article <span className='blue_gradient'>Summary</span>
+              </h2>
+              <div className='summary_box'>
+                <p className='font-inter font-medium text-sm text-gray-700'>{article.summary}</p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
         <button 
           type='button'
-          onClick={() => window.open('https://github.com/distinctmind')}
+          onClick={() => window.open('https://github.com/distinctmind/sumz')}
           className='black_btn'
         >
           Github
